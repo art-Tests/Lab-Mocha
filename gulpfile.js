fd55c330fc748da25c47d3980f3e04fb34c35151
art <art@cowell.com.tw>
2017-06-27 17:48:47 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var chai = require('chai');
+var mocha = require('gulp-mocha');
+var karma = require('karma');
+
+gulp.task('default',["unit"],function(){
+	console.log('My Default Task');
+})
+
+gulp.task('unit',function() {
+	console.log('Run Unit Test With Mocha');
+  return gulp.src(['test/*.test.js'], { read: false })
+    .pipe(mocha({
+      reporter: 'mochawesome',
+      reporterOptions: {
+        reportDir: 'report',
+        reportName: 'UnitTestReport',
+        reportTitle: 'UnitTestReport'
+        //inlineAssets: true
+      },
+      globals: {
+        should: require('chai')
+      }
+    }));
+});
+
+
+gulp.task('karma', function (done) {
+  karma.server.start({
+    configFile: __dirname + '/karma.conf.js'
+  }, done);
+});
